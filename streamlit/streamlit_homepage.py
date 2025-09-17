8255f2e86ac679f4700d53726c0e37be9c9f2b93
umj95 <umj.ninetyfive@gmail.com>
2025-09-17 23:54:41 +0200

    added syspath to find core

--- streamlit/streamlit_homepage.py
+++ streamlit/streamlit_homepage.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 pg = st.navigation([st.Page("pages/Startseite.py"),
                     st.Page("pages/Orgelpredigt_Analyse.py"), 
                     st.Page("pages/Orgelpredigt_Vergleich.py")])
