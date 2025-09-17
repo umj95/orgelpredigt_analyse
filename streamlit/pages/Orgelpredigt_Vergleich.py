db6776b0fe3049e514e56199b8dfb3f495450c3b
umj95 <umj.ninetyfive@gmail.com>
2025-09-17 23:56:28 +0200

    added syspath for core module

--- streamlit/pages/Orgelpredigt_Vergleich.py
+++ streamlit/pages/Orgelpredigt_Vergleich.py
@@ -1,6 +1,8 @@
 import sys
 import os
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import streamlit as st
 from core.utils import Sermon, Person, get_short_info
 from collections import Counter
